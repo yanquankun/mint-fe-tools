dbcfd845bf8af4bf1e6101262c6e640d9301c7a2
yanquankun <yanquankun@58.com>
2024-08-15 16:37:57 +0800

    feat: add git output

--- packages/@mps/cli/bin/mps.js
+++ packages/@mps/cli/bin/mps.js
@@ -3,6 +3,7 @@ const program = require('commander');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
 const version = require('../package.json').version;
+const execa = require('execa');
 
 program
   .name('mpscli')
@@ -36,6 +37,31 @@ program
   .command('clean')
   .description('清除mps配置目录')
   .action(() => console.log(chalk.bgGreen.bgCyan('清除mps配置目录')));
+
+program
+  .command('git')
+  .option('-u, --user', '获取git用户信息')
+  .option('-b, --branch', '获取git分支名')
+  .option('-r, --remote', '获取git远程仓库地址')
+  .description('获取git信息')
+  .action(async (name) => {
+    if (name.user) {
+      const { stdout } = await execa('git', ['config', 'user.name'], { cwd: process.cwd() });
+      console.log(chalk.bgGreen.bgCyan('git user:'), stdout);
+    }
+    if (name.branch) {
+      const { stdout } = await execa('git', ['symbolic-ref', '--short', 'HEAD'], {
+        cwd: process.cwd(),
+      });
+      console.log(chalk.bgGreen.bgCyan('git branch:'), stdout);
+    }
+    if (name.remote) {
+      const { stdout } = await execa('git', ['config', 'remote.origin.url'], {
+        cwd: process.cwd(),
+      });
+      console.log(chalk.bgGreen.bgCyan('git remote:'), stdout);
+    }
+  });
 
 program.on('command:*', ([cmd]) => {
   console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`));
