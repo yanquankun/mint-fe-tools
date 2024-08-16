ed697ddc4324415735de245e99ec2aa71c26aa15
yanquankun <yanquankun@58.com>
2024-08-16 15:31:17 +0800

    update: update type.js

--- packages/@mps/cli/utils/type.js
+++ packages/@mps/cli/utils/type.js
@@ -1,5 +1,31 @@
-exports.isFunction = (v) =>
-  Object.prototype.toString.call(v) === '[object Function]' ||
-  Object.prototype.toString.call(v) === '[object AsyncFunction]';
+const objectToString = Object.prototype.toString;
+const toTypeString = (value) => objectToString.call(value);
 
-exports.isArray = (v) => Array.isArray(v);
+exports.isFunction = (val) => Object.prototype.toString.call(val) === '[object Function]';
+
+exports.isAsyncFunction = (val) => Object.prototype.toString.call(val) === '[object AsyncFunction]';
+
+exports.isArray = (val) => Array.isArray(val);
+
+exports.isMap = (val) => toTypeString(val) === '[object Map]';
+
+exports.isSet = (val) => toTypeString(val) === '[object Set]';
+
+exports.isDate = (val) => toTypeString(val) === '[object Date]';
+
+exports.isRegExp = (val) => toTypeString(val) === '[object RegExp]';
+
+exports.isFunction = (val) => typeof val === 'function';
+
+exports.isString = (val) => typeof val === 'string';
+
+exports.isSymbol = (val) => typeof val === 'symbol';
+
+exports.isObject = (val) => val !== null && typeof val === 'object';
+exports.isPromise = (val) => {
+  return (
+    (this.isObject(val) || this.isFunction(val)) &&
+    this.isFunction(val.then) &&
+    this.isFunction(val.catch)
+  );
+};
