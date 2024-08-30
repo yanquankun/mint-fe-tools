b1f883c39bb513a43316980decf97fa8cd34f2d1
yanquankun <yanquankun@58.com>
2024-08-30 17:27:26 +0800

    feat: test

--- packages/@mps/cli-lbg/index.js
+++ packages/@mps/cli-lbg/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.lbg = {
+  /** 获取lbg-noticeTask模板 */
   getLbgNoticeTaskTemp: (lbgCliPath) =>
     fs.readFileSync(path.join(lbgCliPath, '/templates/lbg-noticeTask.js'), 'utf-8'),
 };
