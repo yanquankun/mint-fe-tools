b5c662147a7ea7243104068a447d235228d3f773
yanquankun <17600610907@163.com>
2024-11-16 17:01:54 +0800

    add: guid

--- packages/@mpsc/cli/utils/common.js
+++ packages/@mpsc/cli/utils/common.js
@@ -31,8 +31,17 @@ function getLocalIP() {
   return '127.0.0.1';
 }
 
+function guid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 module.exports = {
   timestampToTime,
   checkVersion,
   getLocalIP,
+  guid,
 };
